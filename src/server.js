42d80f30ac38c9bfa36fe43154a49e442b07379a
Vitor-16 <vitor.slima1616@gmail.com>
2023-03-15 19:37:31 -0300

    install cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 //ARQUIVO ENDRY POINT DO PROJETO, FAZENDO USO DO EXPRESS 
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 //DEFINIÇÃO DA PORTA LOCAL PARA RODAR O SERVIDOR
-const PORT = 3000;
+const PORT = 3333;
 
 //REQUISIÇÃO DAS ROTAS
 const UsuariosRouter = require('../src/routes/UsuariosRouter');
@@ -12,11 +13,12 @@ const MedicamentosRouter = require('../src/routes/MedicamentosRouter');
 //MIDDLEWARES DE CORPO DE REQUISIÇÃO
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors());
 
 //UTILIZANDO AS ROTAS
 app.use('/', UsuariosRouter); 
 app.use('/', MedicamentosRouter);
 
-app.listen(PORT, ()=> console.log('SERVIDOR RODANDO EM http://localhost:3000'));
+app.listen(PORT, ()=> console.log('SERVIDOR RODANDO EM http://localhost:3333'));
 
 
